9882b0f95c6e11565225a59db2600fc1d507394d
SK Sakib <sksakib85@yahoo.com>
2024-08-27 16:41:49 -0500

    amar naam chodu

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -50,7 +50,7 @@ const AboutDetails = () => {
         >
           <img
             className="w-full h-auto"
-            src={`https://github-readme-stats.vercel.app/api/top-langs?username=sakibskib&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=sakibskib&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
             alt="sakibskib"
             loading="lazy"
           />
@@ -77,8 +77,8 @@ const AboutDetails = () => {
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
           <img
             className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
-            alt="CodeBucks"
+            src={"https://github-readme-streak-stats.herokuapp.com?user=sakibskib&theme=dark&hide_border=true&type=png&background=EB545400&ring=4052DE&currStreakLabel=4052DE"}
+            alt="bs"
             loading="lazy"
           />
         </ItemLayout>
